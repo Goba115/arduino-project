97c23fa81d74319883c3a71829af59d8509c2af2
Gabrijel Peternel <102869281+Goba115@users.noreply.github.com>
2024-01-17 17:53:51 +0100

    Add files via upload

--- funkcijskiGenerator/funkcijskiGenerator_podroben/funkcijskiGenerator_podroben.ino
+++ funkcijskiGenerator/funkcijskiGenerator_podroben/funkcijskiGenerator_podroben.ino
@@ -1,13 +1,14 @@
-int perioda = 2;        // začetna frekvenca      [mS]
-int delovniCikelj = 50; // začetni delovni cikelj [%]
+int perioda = 1000;          // začetna frekvenca      [mS]
+float delovniCikelj = 0.005; // začetni delovni cikelj [%]
+int multiplikator = 100;     // zaradi problema prevelikih št. je dodan multiplikator
 
 int onTime;             //čas visokega stanja
 int offTime;            //čas nizkega stanja
 
-int povecavaP = 10;    // povečanje frekvence
-int povecavaDC = 10;    // povečanje delovnega cikla
-int zmanjsavaP = 10;   // zmanjševanje frekvence
-int zmanjsavaDC = 10;   // zmanjševanje delovnega cikla
+int povecavaP = 100;      // povečanje periode
+float povecavaDC = 0.001; // povečanje delovnega cikla
+int zmanjsavaP = 100;     // zmanjševanje periode
+float zmanjsavaDC = 0.001;  // zmanjševanje delovnega cikla
 
 bool zastavica2 = false; // zastavice so namenjene enkratni ponovitvi kode ob pritisku gumba
 bool zastavica4 = false;
@@ -15,62 +16,75 @@ bool zastavica7 = false;
 bool zastavica8 = false;
 
 void setup() {
-  pinMode(2, INPUT);  // povečava frekvence
+  pinMode(2, INPUT);  // povečava periode
   pinMode(4, INPUT);  // povečava delovnega cikla
   pinMode(5, OUTPUT); // glavni izhod  
   pinMode(7, INPUT);  // pomanjšanje delovnega cikla
-  pinMode(8, INPUT);  // pomanjšanje frekvence
+  pinMode(8, INPUT);  // pomanjšanje periode
 
-  digitalWrite(5, LOW);
-
+  Serial.begin(9600);
 
 }
 
 void loop() {
   
-  onTime = (perioda * delovniCikelj) / 100;  //simpl matematka za izračun visokega stanja
+  onTime = (perioda * delovniCikelj) / multiplikator;  //simpl matematka za izračun visokega stanja
   offTime = perioda - onTime;                //izračun nizkega stanja
 
-  onTime = abs(onTime);
-  offTime = abs(offTime);
+
+  if(onTime < 1){
+    onTime = 1;    
+  }
+
+  
+  if(offTime < 1){
+    offTime = 1;    
+  }
 
   digitalWrite(5, HIGH);          // visoko stanje izhoda
-  delay(onTime);                  // zakasnitev visokega stanja
+  delayMicroseconds(onTime);      // zakasnitev visokega stanja
   digitalWrite(5, LOW);           // nizko stanje izhoda 
-  delay(offTime);                 // zakasnitev nizkega stanja
+  delayMicroseconds(offTime);     // zakasnitev nizkega stanja
 
-  if (digitalRead(2) == HIGH && !zastavica2) { //podaljšujemo periodo
+
+  if (digitalRead(2) == HIGH && !zastavica2){ //podaljšujemo periodo
     perioda += povecavaP;
     zastavica2 = true;
+    Serial.println(onTime);
+    Serial.println(offTime);
   }
   
-  if (digitalRead(8) == HIGH && !zastavica8) { //zmanjšujemo periodo
-      perioda -= zmanjsavaP;        
-      zastavica8 = true;
+  if (digitalRead(8) == HIGH && !zastavica8){  //zmanjšujemo periodo
+    perioda -= zmanjsavaP;        
+    zastavica8 = true;
   }
 
-  if (digitalRead(4) == HIGH && !zastavica4) { //podaljšujemo delovni cikelj
+  if (digitalRead(4) == HIGH && !zastavica4){ //podaljšujemo delovni cikelj
     delovniCikelj += povecavaDC;       
     zastavica4 = true;
   }
-  
-  if (digitalRead(7) == HIGH && !zastavica7) { //zmanjšujemo delovni cikelj
+
+  if (digitalRead(7) == HIGH && !zastavica7){ //zmanjšujemo delovni cikelj
     delovniCikelj -= povecavaDC;
     zastavica7 = true;
   }
 
+
 class zastavce;
-  if (digitalRead(2) == LOW) {
+
+  if (digitalRead(2) == LOW){
     zastavica2 = false;
   }
-  if (digitalRead(4) == LOW) {
+
+  if (digitalRead(4) == LOW){ 
     zastavica4 = false;
   }
-  if (digitalRead(7) == LOW) {
+
+  if (digitalRead(7) == LOW){ 
     zastavica7 = false;
   }
-  if (digitalRead(8) == LOW) {
+
+  if (digitalRead(8) == LOW){ 
     zastavica8 = false;
   }
-
 }
