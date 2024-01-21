933a811a446936aa82aadd79321a964e84224288
Gabrijel Peternel <102869281+Goba115@users.noreply.github.com>
2024-01-21 20:55:44 +0100

    Add files via upload

--- funkcijskiGenerator/funkcijskiGenerator_podroben/funkcijskiGenerator_podroben.ino
+++ funkcijskiGenerator/funkcijskiGenerator_podroben/funkcijskiGenerator_podroben.ino
@@ -1,26 +1,26 @@
-int perioda = 1000;          // začetna frekvenca      [mS]
-float delovniCikelj = 0.005; // začetni delovni cikelj [%]
-int multiplikator = 100;     // zaradi problema prevelikih št. je dodan multiplikator
+long perioda = 1000;      // začetna frekvenca      [mS]
+float delovniCikelj = 50; // začetni delovni cikelj [%]
+int delitelj = 100;       // delitelj, da zračunamo onTime
 
-int onTime;             //čas visokega stanja
-int offTime;            //čas nizkega stanja
+long onTime;              //čas visokega stanja
+long offTime;             //čas nizkega stanja
 
-int povecavaP = 100;      // povečanje periode
-float povecavaDC = 0.001; // povečanje delovnega cikla
-int zmanjsavaP = 100;     // zmanjševanje periode
-float zmanjsavaDC = 0.001;  // zmanjševanje delovnega cikla
+int povecavaP = 500;      // povečanje periode
+int zmanjsavaP = 500;     // zmanjševanje periode
+float povecavaDC = 5;     // povečanje delovnega cikla
+float zmanjsavaDC = 5;    // zmanjševanje delovnega cikla
 
-bool zastavica2 = false; // zastavice so namenjene enkratni ponovitvi kode ob pritisku gumba
+bool zastavica2 = false;  // zastavice so namenjene enkratni ponovitvi kode ob pritisku gumba
 bool zastavica4 = false;
 bool zastavica7 = false;
 bool zastavica8 = false;
 
 void setup() {
-  pinMode(2, INPUT);  // povečava periode
-  pinMode(4, INPUT);  // povečava delovnega cikla
-  pinMode(5, OUTPUT); // glavni izhod  
-  pinMode(7, INPUT);  // pomanjšanje delovnega cikla
-  pinMode(8, INPUT);  // pomanjšanje periode
+  pinMode(2, INPUT);       // povečava periode
+  pinMode(4, INPUT);       // povečava delovnega cikla
+  pinMode(5, OUTPUT);      // glavni izhod  
+  pinMode(7, INPUT);       // pomanjšanje delovnega cikla
+  pinMode(8, INPUT);       // pomanjšanje periode
 
   Serial.begin(9600);
 
@@ -28,17 +28,28 @@ void setup() {
 
 void loop() {
   
-  onTime = (perioda * delovniCikelj) / multiplikator;  //simpl matematka za izračun visokega stanja
-  offTime = perioda - onTime;                //izračun nizkega stanja
+  if (perioda < 250){
+    perioda = 250;
+  }
+
+  onTime = (perioda * delovniCikelj) / delitelj;  //simpl matematka za izračun visokega stanja
+  offTime = perioda - onTime;                     //izračun nizkega stanja
 
 
   if(onTime < 1){
-    onTime = 1;    
+    onTime = 10;    
   }
 
-  
   if(offTime < 1){
-    offTime = 1;    
+    offTime = 10;    
+  }
+
+  if(delovniCikelj >= 95){
+  delovniCikelj = 95;  
+  }
+
+  if(delovniCikelj <= 5){
+  delovniCikelj = 5;  
   }
 
   digitalWrite(5, HIGH);          // visoko stanje izhoda
@@ -50,27 +61,41 @@ void loop() {
   if (digitalRead(2) == HIGH && !zastavica2){ //podaljšujemo periodo
     perioda += povecavaP;
     zastavica2 = true;
+
+    Serial.println("onTime");
     Serial.println(onTime);
+    Serial.println("offTime");
     Serial.println(offTime);
   }
   
   if (digitalRead(8) == HIGH && !zastavica8){  //zmanjšujemo periodo
     perioda -= zmanjsavaP;        
     zastavica8 = true;
+
+    Serial.println("onTime");
+    Serial.println(onTime);
+    Serial.println("offTime");
+    Serial.println(offTime);
   }
 
   if (digitalRead(4) == HIGH && !zastavica4){ //podaljšujemo delovni cikelj
     delovniCikelj += povecavaDC;       
     zastavica4 = true;
+
+    Serial.println("delovni cikelj");
+    Serial.println(delovniCikelj);
   }
 
   if (digitalRead(7) == HIGH && !zastavica7){ //zmanjšujemo delovni cikelj
     delovniCikelj -= povecavaDC;
     zastavica7 = true;
+    
+    Serial.println("delovni cikelj");
+    Serial.println(delovniCikelj);
   }
 
 
-class zastavce;
+class zastavice;
 
   if (digitalRead(2) == LOW){
     zastavica2 = false;
